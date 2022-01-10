e2989e199740b907b2e365215e810885b0e682ab
Austin B <89538206+ksk24@users.noreply.github.com>
2022-01-10 18:18:52 -0500

    Add files via upload

---
+++ BBProject/home.js
@@ -0,0 +1,11 @@
+function Home(){
+    return (
+        <Card
+            txtcolor="black"
+            header="BadBank Landing Page"
+            title="Welcome to the bank"
+            text="You can use this bank"
+            body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+        />
+    );
+}
